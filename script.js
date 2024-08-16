fce9c2fe5b6fb6475c4ceaf16dd1649564c8804f
EMANOEL VITOR VENTURA ATANAZIO <e.atanazio@labcetec.net>
2024-08-16 15:03:23 -0300

    AutoPlay

--- script.js
+++ script.js
@@ -3,14 +3,25 @@ const galeriaControlContainer = document.querySelector('.galeria-control');
 const galeriaControl = ['previous', 'next'];
 const galleryItems = document.querySelectorAll('.gallery-item');
 
-class Carrossel{
-    constructor(container, items, controls){
+
+class Carrossel {
+    constructor(container, items, controls) {
         this.carrosselContainer = container;
         this.carrosselControls = controls;
         this.carrosselArray = [...items];
+        this.mySlideInterval = null;
+        this.mySlideInterval = setInterval(
+            this.autoSlide.bind(this),
+            5000
+        );
     }
 
-    updateGallery(){
+    autoSlide() {
+        this.carrosselArray.push(this.carrosselArray.shift());
+        this.updateGallery();
+    }
+
+    updateGallery() {
         this.carrosselArray.forEach(el => {
             el.classList.remove('gallery-item-1');
             el.classList.remove('gallery-item-2');
@@ -21,31 +32,55 @@ class Carrossel{
         });
 
         this.carrosselArray.slice(0, 6).forEach((el, i) => {
-            el.classList.add(`gallery-item-${i+1}`);
+            el.classList.add(`gallery-item-${i + 1}`);
         });
     }
 
-    setCurrentState(direction){
-        if(direction.className == 'galeria-control-previous'){
+    setCurrentState(direction) {
+        if (direction.className == 'galeria-control-previous') {
             this.carrosselArray.unshift(this.carrosselArray.pop());
-        }else{
+        } else {
             this.carrosselArray.push(this.carrosselArray.shift());
         }
+
+        clearInterval(this.mySlideInterval);
         this.updateGallery();
+        this.mySlideInterval = setInterval(
+            this.autoSlide.bind(this),
+            5000
+        );
     }
-    setControls(){
+    setControls() {
         this.carrosselControls.forEach(controls => {
             galeriaControlContainer.appendChild(document.createElement('button')).className = `galeria-control-${controls}`;
             document.querySelector(`.galeria-control-${controls}`).innerText = controls;
         });
     }
 
-    useControls(){
+    useControls() {
         const triggers = [...galeriaControlContainer.childNodes];
         triggers.forEach(controls => {
             controls.addEventListener('click', e => {
                 e.preventDefault();
-                this.setCurrentState(controls);
+
+                if (controls.className == 'galeria-Control-add') {
+                    const newItem = document.createElement('img');
+                    const latestItem = this.carrosselArray.length;
+                    const latestIndex = this.carrosselArray.findIndex(item => item.getAttribute('data-index') == this.carrosselArray.length)+1;
+          
+                    Object.assign(newItem,{
+                      className: 'gallery-item',
+                      src: `http://fakeimg.pl/300/?text=${this.carrosselArray.length+1}`
+                    });
+                    newItem.setAttribute('data-index', this.carrosselArray.length+1);
+          
+                    this.carrosselArray.splice(latestIndex, 0, newItem);
+                    document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+                    this.updateGallery();
+          
+                  } else {
+                    this.setCurrentState(controls);
+                  }
             });
         });
     }
